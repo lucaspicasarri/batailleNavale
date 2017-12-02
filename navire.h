151f32d357a66abc697d8935b902a46c48f3b1f2
Lucas Picasarri-Arrieta <lucas.picasarri-arrieta@etu.umontpellier.fr>
2017-12-03 00:57:57 +0100

    First Commit

---
+++ navire.h
@@ -0,0 +1,29 @@
+#ifndef _NAVIRE_H
+#define _NAVIRE_H
+
+class Navire {
+
+  int m_nbcases;
+  int** m_T; // correspond au tableau bidimensionnel des coordonnées x,y de chaque case du tableau
+  bool m_coule;
+  bool m_vertical;
+
+  //Constructeurs
+  Navire();
+  Navire(int nbcases, bool vertical, bool coule);
+  bool isCoule()const; //vérifie si un bateau est coulé
+  void rotation (); // pivote le bateau
+
+  //Accesseurs
+  bool getOrientation()const;
+  bool getEtat()const;
+  int getTaille()const;
+
+  //
+  void setOrientation(bool orientation);
+  void setEtat(bool etat); //paramètres constants?
+
+  
+}
+
+#endif
